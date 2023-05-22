9d96ab291c54b557066c6bbe3c2173685f8cbe14
vahidSpectree <vahidseyfollahzadeh@gmaile.com>
2023-05-22 17:19:19 +0430

    first commits

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </AuthContextProvider>
